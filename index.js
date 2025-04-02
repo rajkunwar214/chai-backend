529bd91df147bd3c6be140756efc655c0a05086b
rajkunwar214 <rajkunwar8853@gmail.com>
2025-04-03 01:49:46 +0530

    api

--- index.js
+++ index.js
@@ -31,6 +31,44 @@ app.get('/swati',(req,res)=>
 app.get("/random",(req,res)=>{
   res.send("thiss is random page")
 })
+const harsh = {
+  "login": "harshmaurya27",
+  "id": 137767009,
+  "node_id": "U_kgDOCDYoYQ",
+  "avatar_url": "https://avatars.githubusercontent.com/u/137767009?v=4",
+  "gravatar_id": "",
+  "url": "https://api.github.com/users/harshmaurya27",
+  "html_url": "https://github.com/harshmaurya27",
+  "followers_url": "https://api.github.com/users/harshmaurya27/followers",
+  "following_url": "https://api.github.com/users/harshmaurya27/following{/other_user}",
+  "gists_url": "https://api.github.com/users/harshmaurya27/gists{/gist_id}",
+  "starred_url": "https://api.github.com/users/harshmaurya27/starred{/owner}{/repo}",
+  "subscriptions_url": "https://api.github.com/users/harshmaurya27/subscriptions",
+  "organizations_url": "https://api.github.com/users/harshmaurya27/orgs",
+  "repos_url": "https://api.github.com/users/harshmaurya27/repos",
+  "events_url": "https://api.github.com/users/harshmaurya27/events{/privacy}",
+  "received_events_url": "https://api.github.com/users/harshmaurya27/received_events",
+  "type": "User",
+  "user_view_type": "public",
+  "site_admin": false,
+  "name": "Harsh Maurya",
+  "company": null,
+  "blog": "mauryaharsh464@gmail.com",
+  "location": "Robertsganj  India",
+  "email": null,
+  "hireable": null,
+  "bio": "I am a fullstack mern developer and \r\ncontinuously enhencing my skills",
+  "twitter_username": null,
+  "public_repos": 31,
+  "public_gists": 0,
+  "followers": 1,
+  "following": 3,
+  "created_at": "2023-06-26T08:10:35Z",
+  "updated_at": "2025-03-29T04:33:50Z"
+}
+app.get('harshapi',(req,res)=>{
+  res.json({harsh})
+})
 
 app.listen(process.env.PORT, () => {
   console.log(`Example app listening on git init ${process.env.PORT}`)
